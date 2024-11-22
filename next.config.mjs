a870cdeacfa2cadaed92a82e3328ab76fc9ba84f
vignesh reddy <vignesh.pingili@gmail.com>
2024-11-22 10:28:37 +0530

    next config for secret

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import Google from 'next-auth/providers/google';
+import LinkedIn from 'next-auth/providers/linkedin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    Google,LinkedIn
+  ],
+};
 
 export default nextConfig;
